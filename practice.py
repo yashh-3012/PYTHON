0e25dfba1c2d5e787c335443f20d41fb51699b35
Yash Shukla <yashshukla1506@gmail.com>
2026-06-28 22:43:27 +0530

    new game added

--- practice.py
+++ practice.py
@@ -28,13 +28,39 @@ def odd_even():
             print('YOU WIN !!!\n','Computer\'s number:',a,'Your number:',b,'Result:',c)
         else:
             print('YOU LOSE !!!\n','Computer\'s number:',a,'Your number:',b,'Result:',c)
+def number_guessing():
+    number=random.randint(1,100)
+    attempts=0
+    while True: 
+        if attempts==5:
+            print('YOU LOST!!!\nYou have used all your attempts.')
+            break
+        guess=int(input('Guess the number between 1 and 100: '))
+        attempts+=1
+        if guess<1 or guess>100:
+            print('Number out of range')
+            continue
+        if guess<number:
+            print('Too low!')
+        elif guess>number:
+            print('Too high!')
+        if guess!=number:
+            print('Try again!')
+    if guess==number:
+        print('Congratulations! You guessed the number in',attempts,'attempts.')
+    else:
+        print('The number was:',number)
 while True:
-    print('1. Finger Cricket\n2. Odd Even')
+    print('1. Finger Cricket\n2. Odd Even\n3. Number Guessing')
     choice=int(input('which game do you want to play?'))
     if choice==1:
         finger_cricket()
     elif choice==2:
         odd_even()
+    elif choice==3:
+        number_guessing()   
     else:
         print('Invalid choice')
+
+
 
